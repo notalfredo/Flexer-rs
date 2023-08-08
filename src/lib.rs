e3212ea6db9ffaa843e963550b704bfb7a187117
Alfredo <icealfredo123@gmail.com>
2023-08-08 16:39:49 -0500

    added todo

--- src/lib.rs
+++ src/lib.rs
@@ -19,18 +19,23 @@ impl Dfa {
 		}
 	}
 
-	fn new(self, init_string: &str) -> Self {
+	fn look_up_where_to_insert_node(self, node_to_insert: Node) -> u32 {
+		todo!("Function should tell us where to insert the node")
+	}
+
+	pub fn new(self, init_string: &str) -> Self {
 		Self {
 			node_count: 0,
 			table: Vec::with_capacity(100),
 		}
 	}
 
-	fn new_regex(&mut self, regex_string: &str) {
+	pub fn new_regex(&mut self, regex_string: &str) {
 		let first_char = match regex_string.chars().nth(0) {
 			Some(character) => character,
 			None => panic!("Unable to parse string"),
 		};
 		let init_node: Node = Dfa::new_node(self.node_count + 1);
+		todo!("we should check where to insert our node, insert it then do the same for the rest of character");
 	}
 }
