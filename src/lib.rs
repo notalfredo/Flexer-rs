00a24a51d8831197fe966fb1800690c6819397d7
Alfredo <icealfredo123@gmail.com>
2023-08-08 16:18:19 -0500

    redesigned graph

--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,28 @@
 #[doc = include_str!("../README.md")]
+use std::collections::HashMap;
+
+struct Node {
+	node_number: u32,
+	edges: HashMap<char, Option<Box<Node>>>,
+}
 
 struct Dfa {
-	character: char,
-	next: Vec<Option<Box<Dfa>>>,
+	table: Vec<Option<Box<Node>>>,
 }
 
 impl Dfa {
-	fn new_node(dfa_character: char) -> Self {
-		Self {
-			character: dfa_character,
-			next: Vec::with_capacity(1)
+	fn new_node(num: u32) -> Node {
+		Node {
+			node_number: num,
+			edges: HashMap::new(),
 		}
 	}
 
-	fn new(&mut self, init_string: &str) {
-		let head: Box<Dfa> = Box::new(Dfa::new_node(match init_string.chars().nth(0) {
-			Some(char) => char,
-			None => panic!("Unable to parse init string"),
-		}));
-
-        //Todo: for each following character in our string
-        //create a next node
-        for c in init_string.chars().skip(1) {
-            match self.next[0] {
-                Some( ref mut x ) => println!("fdasf"),
-                None => println!("fdasf")
-            }
-        }
-        /*
-            match self.head {
-            None => self.head = Some(new_node),
-            Some(ref mut node) => {
-                let mut current = node;
-                while let Some(ref mut next_node) = current.next {
-                    current = next_node;
-                }
-                current.next = Some(new_node);
-            }
-        }
-        */
-
+	fn new(self, init_string: &str) -> Self {
+		Self {
+			table: Vec::with_capacity(100),
+		}
 	}
+
+	fn new_regex(regex_string: &str) {}
 }
-
-
-
-
-
-
-/*
-    let temp = head;
-    while temp.next != null {
-        temp = temp.next
-    }
-*/
