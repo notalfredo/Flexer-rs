8b3f558d0f97705188a2456330ef6c1541232370
Alfredo <icealfredo123@gmail.com>
2023-08-08 16:34:09 -0500

    init scetch

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ struct Node {
 }
 
 struct Dfa {
+	node_count: u32,
 	table: Vec<Option<Box<Node>>>,
 }
 
@@ -20,9 +21,16 @@ impl Dfa {
 
 	fn new(self, init_string: &str) -> Self {
 		Self {
+			node_count: 0,
 			table: Vec::with_capacity(100),
 		}
 	}
 
-	fn new_regex(regex_string: &str) {}
+	fn new_regex(&mut self, regex_string: &str) {
+		let first_char = match regex_string.chars().nth(0) {
+			Some(character) => character,
+			None => panic!("Unable to parse string"),
+		};
+		let init_node: Node = Dfa::new_node(self.node_count + 1);
+	}
 }
